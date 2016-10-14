a1fb7d13b7727fbd67e109df506e8bb9125c890d
chandra <pcylus@gmail.com>
2016-10-14 07:00:26 -0500

    adding new files 02_variables

---
+++ 02_variables/01_var-asignments.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+priyanka = True
+print (counter)
+print (miles)
+print (name)
+print ("Priyanaks' value ", priyanka)
+
+#Multiple assigmentns
+a = b = c = 1
+print("Value of a ", a)
+print("Value of b", b)
+print("Value of c", c)
+
+print("")
+x, y, str = 1, 2, "john"
+print("Value of x ", x)
+print("Value of y", y)
+print("Value of str", str)
---
+++ 02_variables/02_del-references.py
@@ -0,0 +1,11 @@
+# delete a references
+var1 = 1
+var2 = 10
+
+print (var1)
+print (var2)
+
+del var1 # delete the reference var1
+
+print(var1)
+print(var2)
